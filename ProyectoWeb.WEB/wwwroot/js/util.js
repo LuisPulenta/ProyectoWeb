b6c49223d71a448f3d9576d18d092773158faeae
LuisPulenta <luisalbertonu@gmail.com>
2024-01-01 23:54:30 -0300

    Grabar a Excel

---
+++ ProyectoWeb.WEB/wwwroot/js/util.js
@@ -0,0 +1,8 @@
+﻿function DescargarExcel(nombreArchivo, base64Archivo) {
+    const link = document.createElement("a");
+    link.download = nombreArchivo;
+    link.href = "data:aplication/vnd.ms-excel;base64," + base64Archivo;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
